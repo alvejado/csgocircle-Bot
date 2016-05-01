533265b1067d4d712a2073e3ca272b94ba780111
Paulo Abreu <cabasafado2@hotmail.com>
2016-05-01 15:35:22 -0300

    optimization

--- bot.js
+++ bot.js
@@ -1,31 +1,26 @@
-document.getElementById("play-points").value=1;
-var resulter = 1;
+document.getElementById("play-points").value=1; //start bet with 1
+
+var resulter = 0;//
 
 var loop = window.setInterval(function() {
-    if(document.getElementById("rolled-text-1").className == "rolled-text hidden-xs hidden-sm hidden-md"){
-        var result = document.getElementById("rolled-number").innerHTML;
-        if(result == 11 || result == 10 || result == 9 || result == 8 || result == 14 || result == 13 || result == 12 || result == 0){
-            resulter = 0;
-        }else{  
-            document.getElementById("play-points").value=1;
-            resulter = 1;
-        }
+    var rooled_text_1 = document.getElementById("rolled-text-1").className;
+    if(rooled_text_1 == "rolled-text hidden-xs hidden-sm hidden-md"){ //check if the round ended
+        var result = document.getElementById("rolled-number").innerHTML; //get result
         
+        if(result == 11 || result == 10 || result == 9 || result == 8 || result == 14 || result == 13 || result == 12 || result == 0){
+            document.getElementById("play-points").value=document.getElementById("play-points").value*2+1;
+            resulter = 1; //lost, then change this var for after to bet (bet*2+1)
+        }else{  
+            document.getElementById("play-points").value=1; //won, then change bet for initial value
+            resulter = 0; //restart
+        }  
     }
     
 }, 4000);
 
-var fazAposta = window.setInterval(function() {
-    var tempo = document.getElementById("countdown-timer").innerHTML;
-    
+var tobet = window.setInterval(function() {
+    var tempo = document.getElementById("countdown-timer").innerHTML; //get time 
     if(tempo < 25.0 && tempo > 24.0){
-        if(resulter == 0){
-            document.getElementById("play-points").value=document.getElementById("play-points").value*2+2;
-            system.play.bet.red();
-            resulter = 1;
-        }else{
-            system.play.bet.red();
-        }
-        
+            system.play.bet.red();  
     }
-}, 100);
+}, 500);
