fb467795d71c1cfb08ec4f8fc7cd94e004cc7f98
Paulo Abreu <cabasafado2@hotmail.com>
2016-05-01 19:55:44 -0300

    menu added and others...

--- bot.js
+++ bot.js
@@ -1,6 +1,15 @@
-document.getElementById("play-points").value=1; //start bet with 1
+// Select the element in DOM
+var $wrapper = document.querySelector('.wrapper'),
 
-var resulter = 0;//
+//HTML code
+ newHTML = '<div style="background:#FFF;position:fixed;bottom:0px;right:0px;color:#000;border-radius:2px;">Initial Value: <input type="text" id="initial_value" value="1"><br/><input type="radio" name="color" value="red" disabled checked> Red - <input type="radio" name="color" value="black" disabled> Black</div>';
+ 
+// Insert HTML
+$wrapper.insertAdjacentHTML('afterbegin', newHTML);
+
+var initial_value = document.getElementById("initial_value").value;
+
+document.getElementById("play-points").value=initial_value; //start bet with 1
 
 var loop = window.setInterval(function() {
     var rooled_text_1 = document.getElementById("rolled-text-1").className;
@@ -9,10 +18,11 @@ var loop = window.setInterval(function() {
         
         if(result == 11 || result == 10 || result == 9 || result == 8 || result == 14 || result == 13 || result == 12 || result == 0){
             document.getElementById("play-points").value=document.getElementById("play-points").value*2+1;
-            resulter = 1; //lost, then change this var for after to bet (bet*2+1)
+            //lost, then change this var for after to bet (bet*2+1)
         }else{  
-            document.getElementById("play-points").value=1; //won, then change bet for initial value
-            resulter = 0; //restart
+            document.getElementById("play-points").value=initial_value; //won, then change bet for initial value
+            document.getElementsByName("color")[0].disabled = false;
+            document.getElementsByName("color")[1].disabled = false;
         }  
     }
     
@@ -21,6 +31,12 @@ var loop = window.setInterval(function() {
 var tobet = window.setInterval(function() {
     var tempo = document.getElementById("countdown-timer").innerHTML; //get time 
     if(tempo < 25.0 && tempo > 24.0){
-            system.play.bet.red();  
+        if(document.getElementsByName("color")[0].checked == true){
+            system.play.bet.black();
+        }else{
+            system.play.bet.red();
+        }
+            document.getElementsByName("color")[0].disabled = true;
+            document.getElementsByName("color")[1].disabled = true;
     }
-}, 500);
+}, 600);
